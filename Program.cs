6270ce6c8a53334fc4004fd235bea17e091d181f
Albatov-start <t717538@gmail.com>
2026-09-13 20:44:26 +0600

    Finished Hvatun battle

--- Program.cs
+++ Program.cs
@@ -1,19 +1,48 @@
 using System;
 using System.Threading;
 
-int health = 150;
+int stamina = 300;
 int distance = 5;
+int PlayerHealth = 100;
 
-Console.WriteLine( "[START] Hvatun spawned with " + health + " HP!" );
-Console.WriteLine( " [START] Sound System: Loud roar played!" );
-Console.WriteLine("------------------------------------------");
-while ( distance > 0)
+Console.WriteLine("[START] Hvatun spawned!");
+Console.WriteLine("[START] Hvatun has 300 stamina!");
+Console.WriteLine("[START] Sound System: Loud roar played!");
+Console.WriteLine("-------------------------------------");
+
+while (distance > 1)
 {
-    Console.WriteLine( "[UPDATE] Checking distance... Current: " + distance + "m");
+    Console.WriteLine("[UPDATE] Checking distance... Current: " + distance + "m");
     distance = distance - 1;
-    if (distance < 2)
-    {
-         Console.WriteLine("[UPDATE] STATUS: ATTACK!");
-         break;
-    }
+    Thread.Sleep(400); 
 }
+
+Console.WriteLine("\n[UPDATE] STATUS: ATTACK! Hvatun reached you!");
+
+while (PlayerHealth > 0)
+{
+    PlayerHealth = PlayerHealth - 35;
+    stamina = stamina - 100;
+
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine("[ATTACK] Hvatun bites you for 35 damage!");
+    Console.WriteLine("[INFO] Hvatun stamina left: " + stamina + " stamina");
+    
+    if (PlayerHealth < 0) 
+    {
+        PlayerHealth = 0; 
+    }
+    Console.WriteLine("[WARNING] You have now " + PlayerHealth + " HP");
+
+    Thread.Sleep(600); 
+}
+
+Console.WriteLine("\n================================");
+Console.WriteLine("[GAME OVER] Hvatun killed you!");
+Console.WriteLine("================================");
+
+
+
+
+
+
